2fe3e925e700f2c76649ea9a39f00753907321db
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-01-11 18:24:48 +0200

    Configured media files settings

--- WorkPal/settings.py
+++ WorkPal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +128,6 @@ STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'authentication_app.CustomUser'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
