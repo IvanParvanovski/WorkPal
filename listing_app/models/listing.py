e1590369f48a829b3682138029f393f07f3ba71a
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-02-18 18:21:23 +0200

    Added new listing app to manage the shared fields and functionality between job offer and project

---
+++ listing_app/models/listing.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=150, blank=False, null=False)
+    location = models.CharField(max_length=250, blank=False, null=False)
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+    images = models.URLField()
+    description = models.TextField(blank=False, null=False)
+
+    def __str__(self):
+        return self.title
