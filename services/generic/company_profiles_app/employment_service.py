9d534a41fcadb87a30418360af808915d0056d18
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-02-28 17:23:47 +0200

    Implement employment service

---
+++ services/generic/company_profiles_app/employment_service.py
@@ -0,0 +1,57 @@
+from accounts_app.models.profile import Profile
+from company_profiles_app.models import Employment, Company
+from services.interfaces.company_profiles_app.employment_interface import EmploymentInterface
+
+
+class EmploymentService(EmploymentInterface):
+    @staticmethod
+    def create_employment(profile: Profile, company: Company, job_title: str)\
+            -> Employment:
+
+        employment = Employment(
+            profile=profile,
+            company=company,
+            job_title=job_title,
+            is_associate=False
+        )
+
+        employment.save()
+        return employment
+
+    @staticmethod
+    def get_all_employments():
+        return Employment.objects.all()
+
+    @staticmethod
+    def get_employment_by_id(_id: int):
+        return Employment.objects.get(id=_id)
+
+    @staticmethod
+    def delete_employment(employment):
+        employment.delete()
+
+    @staticmethod
+    def delete_employment_by_id(_id: int):
+        employment = EmploymentService.get_employment_by_id(_id=_id)
+        employment.delete()
+
+    @staticmethod
+    def set_employment_is_associate_to_true(employment: Employment):
+        employment.is_associate = True
+        return employment
+
+    @staticmethod
+    def edit_employment_by_id(
+            _id: int,
+            profile: Profile,
+            company: Company,
+            job_title: str) -> Employment:
+
+        employment = EmploymentService.get_employment_by_id(_id=_id)
+
+        employment.profile = profile
+        employment.company = company
+        employment.job_title = job_title
+
+        return employment
+
---
+++ services/interfaces/company_profiles_app/employment_interface.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+
+from accounts_app.models.profile import Profile
+from company_profiles_app.models import Employment, Company
+
+
+class EmploymentInterface(ABC):
+    @staticmethod
+    @abstractmethod
+    def create_employment(profile: Profile, company: Company, job_title: str) \
+            -> Employment:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def get_all_employments():
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def get_employment_by_id(_id: int):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def delete_employment(employment):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def delete_employment_by_id(_id: int):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def set_employment_is_associate_to_true(employment: Employment):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def edit_employment_by_id(
+            _id: int,
+            profile: Profile,
+            company: Company,
+            job_title: str) -> Employment:
+        pass
