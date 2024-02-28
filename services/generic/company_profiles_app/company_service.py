ace83fd745709565467f9436d656a3bcb2d5f6fd
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-02-28 17:22:54 +0200

    Implement company service

---
+++ services/generic/company_profiles_app/company_service.py
@@ -0,0 +1,50 @@
+from company_profiles_app.models import Company
+from services.interfaces.company_profiles_app.company_interface import CompanyInterface
+
+
+class CompanyService(CompanyInterface):
+    @staticmethod
+    def create_company(address: str,
+                       secondary_address: str,
+                       name: str,
+                       website: str):
+
+        company = Company(
+            address=address,
+            secondary_address=secondary_address,
+            name=name,
+            website=website
+        )
+
+        company.save()
+        return company
+
+    @staticmethod
+    def get_company_by_id(_id: int):
+        return Company.objects.get(id=_id)
+
+    @staticmethod
+    def delete_company_by_id(_id: int):
+        company = CompanyService.get_company_by_id(_id)
+        company.delete()
+
+    @staticmethod
+    def delete_company(company: Company):
+        company.delete()
+
+    @staticmethod
+    def edit_company(_id: int,
+                     address: str,
+                     secondary_address: str,
+                     name: str,
+                     website: str):
+
+        company = CompanyService.get_company_by_id(_id)
+
+        company.address = address
+        company.secondary_address = secondary_address
+        company.name = name
+        company.website = website
+
+        company.save()
+        return company
