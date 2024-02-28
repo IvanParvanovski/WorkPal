982a32c5209fae392becbdde68e1aa62ebd421e0
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-02-28 17:20:43 +0200

    Implement job offer details service

---
+++ services/generic/application_app/job_offer_details_service.py
@@ -0,0 +1,36 @@
+from application_app.models.job_offer_details import JobOfferDetails
+from services.interfaces.application_app.job_offer_details_interface import JobOfferDetailsInterface
+
+
+class JobOfferDetailsService(JobOfferDetailsInterface):
+    @staticmethod
+    def create_job_offer_details(cv, motivation_letter: str) -> JobOfferDetails:
+        job_offer_details = JobOfferDetails.objects.create(cv=cv, motivation_letter=motivation_letter)
+        job_offer_details.save()
+
+        return job_offer_details
+
+    @staticmethod
+    def get_all_job_offer_details():
+        return JobOfferDetails.objects.all()
+
+    @staticmethod
+    def get_job_offer_details_by_id(_id: int) -> JobOfferDetails:
+        return JobOfferDetails.objects.get(id=_id)
+
+    @staticmethod
+    def delete_job_offer_details(job_offer_details: JobOfferDetails):
+        job_offer_details.delete()
+
+    @staticmethod
+    def delete_job_offer_by_id(_id: int):
+        job_offer_details = JobOfferDetailsService.get_job_offer_details_by_id(_id=_id)
+        job_offer_details.delete()
+
+    @staticmethod
+    def edit_job_offer_by_id(_id: int, motivation_letter) -> JobOfferDetails:
+        job_offer_details = JobOfferDetailsService.get_job_offer_details_by_id(_id=_id)
+        job_offer_details.motivation_letter = motivation_letter
+        job_offer_details.save()
+
+        return job_offer_details
