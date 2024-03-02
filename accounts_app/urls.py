565e53150dcf94d89ab679d9777905cad4ba0ef6
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-03-02 20:56:20 +0200

    Added routing for the authentication views

--- accounts_app/urls.py
+++ accounts_app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from accounts_app.views import SignUpCreateView
-
-# from accounts_app.views import SignInView, SignUpView
+from accounts_app.views import SignUpView, SignInView, sign_out_view
 
 urlpatterns = [
-    # path('sign_in/', SignInView.as_view(), name='sign_in'),
-    path('sign_up/', SignUpCreateView.as_view(), name='sign_up'),
+    path('sign_in/', SignInView.as_view(), name='sign_in'),
+    path('sign_up/', SignUpView.as_view(), name='sign_up'),
+    path('sign_out', sign_out_view, name='sign_out')
 ]
