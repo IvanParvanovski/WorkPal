5dbcbdb506513a0f0ceb73d5513aa2a93bacc72e
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-03-09 13:38:32 +0200

    Added view for displaying all job offers - catalog

---
+++ listing_app/views/job_offers_catalog.py
@@ -0,0 +1,12 @@
+from django.views.generic import TemplateView
+
+from services.generic.listing_app.job_offer_service import JobOfferService
+
+
+class JobOffersCatalog(TemplateView):
+    template_name = 'listing_app/job_offer_catalog.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['job_offers'] = JobOfferService.get_all_job_offers()
+        return context
