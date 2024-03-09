9581fdb26800dfe873b462b09129e0e7b50a878c
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-03-09 23:30:07 +0200

    Settup basic configuration for testing theintegration of the dashboard_app into the project

--- dashboard_app/urls.py
+++ dashboard_app/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+
+from dashboard_app.views.dashboard import render_dashboard
+from dashboard_app.views.user_projects import UserProjectsView
+from listing_app.urls import dashboard_urls
 
 urlpatterns = [
-    # path('dashboard/', ),
+    path('', render_dashboard, name='dashboard'),
+    path('', include(dashboard_urls)),
+    path('projects/', UserProjectsView, name='user_projects'),
+
 ]
