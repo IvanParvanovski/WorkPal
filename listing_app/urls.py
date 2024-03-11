19d63bd20ddaf09cd38e50176a2c8a7a174b386a
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-03-11 22:20:43 +0200

    Registered new views for edit and delete of job offers and projects

--- listing_app/urls.py
+++ listing_app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from listing_app.views.create_job_offer import CreateJobOfferView
 from listing_app.views.create_project import CreateProjectView
+from listing_app.views.delete_job_offer import DeleteJobOfferView
+from listing_app.views.delete_project import DeleteProjectView
+from listing_app.views.edit_job_offer import EditJobOfferView
+from listing_app.views.edit_project import EditProjectView
 from listing_app.views.job_offer_detail import JobOfferDetailView
 from listing_app.views.job_offers_catalog import JobOffersCatalog
 from listing_app.views.project_detail import ProjectDetailView
@@ -10,7 +14,12 @@ from listing_app.views.projects_catalog import ProjectsCatalog
 # from listing_app.views import test_slider
 dashboard_urls = [
     path('projects/create_project/', CreateProjectView.as_view(), name='create_project'),
+    path('projects/edit_project/<int:project_id>', EditProjectView.as_view(), name='edit_project'),
+    path('projects/delete_project/<int:project_id>', DeleteProjectView.as_view(), name='delete_project'),
+
     path('job_offers/create_job_offer/', CreateJobOfferView.as_view(), name='create_job_offer'),
+    path('job_offers/edit_job_offer/<int:job_offer_id>', EditJobOfferView.as_view(), name='edit_job_offer'),
+    path('job_offers/delete_job_offer/<int:job_offer_id>', DeleteJobOfferView.as_view(), name='delete_job_offer'),
 ]
 
 main_urls = [
