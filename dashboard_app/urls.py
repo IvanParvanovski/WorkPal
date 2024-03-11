cbce74f4a72352bb73e435b2551ee021d481a2b2
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-03-11 09:45:18 +0200

    Register the new views to the dashboard urls

--- dashboard_app/urls.py
+++ dashboard_app/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path, include
 
 from dashboard_app.views.dashboard import render_dashboard
+from dashboard_app.views.user_companies import UserCompaniesView
+from dashboard_app.views.user_job_offers import UserJobOffersView
 from dashboard_app.views.user_projects import UserProjectsView
 from listing_app.urls import dashboard_urls
 
 urlpatterns = [
     path('', render_dashboard, name='dashboard'),
     path('', include(dashboard_urls)),
-    # path('projects/', UserProjectsView, name='user_projects'),
 
+    path('projects/', UserProjectsView.as_view(), name='user_projects'),
+    path('job_offers/', UserJobOffersView.as_view(), name='user_job_offers'),
+    path('companies/', UserCompaniesView.as_view(), name='user_companies'),
+
+    # path('projects/', UserProjectsView, name='user_projects'),
 ]
