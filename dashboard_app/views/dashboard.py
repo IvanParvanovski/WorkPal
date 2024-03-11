5af9ee4d0ee81601c2f39e6bf65ac7d033a3f055
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-03-11 13:07:42 +0200

    Fixed issue of passing the arguments to the function, profile was given when profile id was required

--- dashboard_app/views/dashboard.py
+++ dashboard_app/views/dashboard.py
@@ -10,13 +10,20 @@ from services.generic.listing_app.project_service import ProjectService
 def render_dashboard(request):
 
     profile_id = request.user.profile.id
+    user_companies = CompanyService.get_companies_by_profile_id(_id=profile_id)
+
+    user_companies_job_offers = [
+        JobOfferService.get_job_offers_by_company_id(company_id=c.id)
+        for c in user_companies
+    ]
 
     context = {
         # 'companies': EmploymentService.get_associated_companies_by_profile_id(profile_id), # THE COMPANIES YOU ARE ASSOCIATED WITH
 
         'association_requests': EmploymentService.get_association_requests_by_company_id(5), # THE ASSOCIATION REQUESTS PEOPLE HAVE MADE TO A SPECIFIC COMPANY
         'application_status': ApplicationService.get_applications_by_profile_id(profile_id), # THE APPLICATIONS YOU HAVE MADE TO PROJECTS OR JOB OFFERS
-        'applications_to_projects': ApplicationService.get_applications_to_user_project(3)
+        'applications_to_projects': ApplicationService.get_applications_to_user_project(3),
+        'user_companies_job_offers': user_companies_job_offers,
     }
 
     print(ApplicationService.get_applications_to_user_project(3))
--- dashboard_app/views/user_job_offers.py
+++ dashboard_app/views/user_job_offers.py
@@ -11,7 +11,7 @@ class UserJobOffersView(TemplateView):
         context = super().get_context_data(**kwargs)
 
         profile = self.request.user.profile
-        user_companies = CompanyService.get_companies_by_profile_id(profile)
+        user_companies = CompanyService.get_companies_by_profile_id(_id=profile.id)
         companies_job_offers = [
             JobOfferService.get_job_offers_by_company_id(company_id=c.id)
             for c in user_companies
