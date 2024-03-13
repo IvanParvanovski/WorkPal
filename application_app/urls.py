a13d4088fb61e5b1fa5259d1a48c467fba8a03ef
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-03-13 14:04:04 +0200

    Display project and job offer details in the dashboard

--- application_app/urls.py
+++ application_app/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 
 from application_app.views import JobOfferApplicationDetailsView, ProjectApplicationDetailsView, AcceptApplication, \
-    RejectApplication
+    RejectApplication, ApplicationJobOfferDetails, ApplicationProjectDetails
 
 urlpatterns = [
     # dashboard
     path('accept_application/<int:application_id>', AcceptApplication.as_view(), name='accept_application'),
     path('reject_application/<int:application_id>', RejectApplication.as_view(), name='reject_application'),
+    path('details_application/job_offer/<int:application_id>',
+         ApplicationJobOfferDetails.as_view(), name='job_offer_application_details'),
+    path('deatils_application/project/<int:application_id>',
+         ApplicationProjectDetails.as_view(), name='project_application_details'),
 
-    path('create_job_offer_application/<int:pk>', JobOfferApplicationDetailsView.as_view(), name='create_job_offer_application'),
-    path('create_project_application/<int:pk>', ProjectApplicationDetailsView.as_view(), name='create_project_application'),
+    path('create_job_offer_application/<int:pk>', JobOfferApplicationDetailsView.as_view(),
+         name='create_job_offer_application'),
+    path('create_project_application/<int:pk>', ProjectApplicationDetailsView.as_view(),
+         name='create_project_application'),
 ]
