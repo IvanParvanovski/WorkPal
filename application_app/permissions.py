d27e527eeb735c7caa9eb84b97dc394a8b70d5ab
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-03-16 12:15:40 +0200

    Fixed permissions bug

--- application_app/permissions.py
+++ application_app/permissions.py
@@ -1,11 +1,13 @@
+from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.models import Group
 from django.shortcuts import redirect
 
 from services.generic.accounts_app.profile_service import ProfileService
 
 
+@permission_required('company_profiles_app.give_rights', raise_exception=True)
 def make_application_moderator(reqeust, *args, **kwargs):
-    profile_id = kwargs.get('profile_id')
+    profile_id = kwargs.get('user_to_grant_rights_id')
 
     user_to_grant_rights = ProfileService.get_profile_by_id(profile_id).user
     application_moderator_group = Group.objects.get(name='ApplicationModerator')
