e60675017f45aceade165f7fe4452ded6474f7af
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-03-24 11:07:42 +0200

    Implemented listing catalogs styles

--- static/js/shared/clickable-div.js
+++ static/js/shared/clickable-div.js
@@ -1,9 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var clickableDivs = document.querySelectorAll('.clickable-div');
-    clickableDivs.forEach(function (div) {
-        div.addEventListener('click', function () {
-            var url = this.getAttribute('data-url');
-            window.location.href = url;
-        });
+var clickableDivs = document.querySelectorAll('.clickable-div');
+clickableDivs.forEach(function (div) {
+    div.addEventListener('click', function () {
+        var url = this.getAttribute('data-url');
+        window.location.href = url;
     });
 });
