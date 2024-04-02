def78a950a7f1e473f115aa06889d6684faf6ca9
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-04-02 10:00:46 +0300

    Fixed issue with visualising the associates when there are no associates

--- dashboard_app/views/user_associates.py
+++ dashboard_app/views/user_associates.py
@@ -13,4 +13,17 @@ class UserAssociatesView(LoginRequiredMixin, TemplateView):
         companies = CompanyService.get_user_companies(profile_id=self.request.user.profile.id)
 
         context['companies_associates'] = EmploymentService.get_associates_for_companies(companies)
+        context['has_associates'] = self.has_associates(context['companies_associates'])
+        print(context['has_associates'])
         return context
+
+    def has_associates(self, companies_associates):
+        print(companies_associates)
+        if len(companies_associates) == 0:
+            return False
+
+        for ca in companies_associates:
+            if not ca.exists():
+                return False
+
+        return True
